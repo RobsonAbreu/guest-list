505d9b6383683f419fc818c4c39e988f12efc795
robsonabreu <robsonabreu.m@gmail.com>
2015-11-17 23:17:54 +0000

    Avaliação Guest-list

---
+++ api/guestlist/guestListService.php
@@ -0,0 +1,54 @@
+<?php
+
+class guestListService {
+    
+    public static function guestList() {
+        $db = ConnectionFactory::getDB();
+        $guests = array();
+        
+        foreach($db->guests() as $guest) {
+           $guests[] = array (
+               'id' => $guest['id'],
+               'name' => $guest['name'],
+               'email' => $guest['email']
+           ); 
+        }
+        return $guests;
+    }
+    
+    public static function getById($id) {
+        $db = ConnectionFactory::getDB();
+        return $db->guests[$id];
+    }
+    
+    public static function add($newGuest) {
+        $db = ConnectionFactory::getDB();
+        $guest = $db->guests->insert($newGuest);
+        return $guest;
+    }
+    
+    public static function update($updatedGuest) {
+        $db = ConnectionFactory::getDB();
+        $guest = $db->guests[$updatedlist['id']];
+        
+        if($guest) {
+            $guest['name'] = $updatedGuest['name'];
+            $guest['email'] = $updatedGuest['email'];
+            $guests->update();
+            return true;
+        }
+        
+        return false;
+    }
+    
+    public static function delete($id) {
+        $db = ConnectionFactory::getDB();
+        $guest = $db->guests[$id];
+        if($guest) {
+            $guest->delete();
+            return true;
+        }
+        return false;
+    }
+}
+?>
---
+++ api/index.php
@@ -0,0 +1,57 @@
+<?php
+
+require 'vendor/autoload.php';
+require 'ConnectionFactory.php';
+require 'guestlist/guestListService.php';
+
+$app = new \Slim\Slim();
+
+
+$app->get('/guests', function() use ( $app ) {
+    
+    $guests = GuestListService::guestList();
+   $app->response()->header('Content-Type','application/json');
+    echo json_encode($guests);
+
+    
+});
+                                                                                                                                                                                                                  
+    $app->get('/guests/:id', function($id) use ($app){
+    $guests = getGuests();
+    $index = array_search($id,array_column($guests,'id'));
+    
+    if ($index > -1){
+        $app->response()->header('Content-Type','application/json');
+        echo json_encode($guests[$index]);
+    }
+     else {
+        $app->response()->setStatus(204);
+        echo "Not found";
+    }
+});
+
+$app->post('/guests', function() use ( $app ) {
+    $guestJson = $app->request()->getBody();
+    $guest = json_decode($guestJson);
+    if($guest) {
+        echo "{$guest->description} added";
+    }
+    else {
+        $app->response()->setStatus(400);
+        echo "Malformat JSON";
+    }
+    
+   
+});
+
+$app->put('/guests/:id', function($id) use ( $app ) {
+    echo $app->request()->getBody();
+});
+
+$app->delete('/guests/:id', function($id) use ( $app ) {
+    echo $id;
+});
+
+$app->run();
+
+?>
